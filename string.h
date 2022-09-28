20e836f87ddfbebf095750d27d0d53106481f6a4
Anton <ivnvtosh@icloud.com>
2022-09-28 19:13:00 +0300

    Init branch main

---
+++ string.h
@@ -0,0 +1,10 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+
+typedef char* string_t;
+
+size_t	my_strlen(string_t);
+
+#endif // STRING_H
---
+++ strlen.c
@@ -0,0 +1,13 @@
+#include "string.h"
+
+size_t	my_strlen(string_t string)
+{
+	size_t	i;
+
+	i = 0;
+	while (string[i] != '\0')
+	{
+		i += 1;
+	}
+	return i;
+}
